baa2c03526cc66c3e5d4c251b8fa5c2cdeb3a326
nanaknihal <nanaknihal@gmail.com>
2022-08-08 15:20:27 -0400

    JS wrapper for creating circuits with different parameters

--- nameThis.js
+++ nameThis.js
@@ -1,7 +1,8 @@
 const { initialize } = require('zokrates-js')
 const fs = require("fs");
 const path = require("path");
-const options = {
+const { assert } = require('console');
+const compileOptions = {
     location: "jsVersion.zok", // location of the root module
     resolveCallback: (from, to) => {
         console.log(from + ' is importing ' + to);
@@ -11,7 +12,59 @@ const options = {
     }
 };
 
+const jwt = 'eyJraWQiOiJvYWdZIn0.eyJjcmVkcyI6IlByb3RvY29sV3RmIiwiYXVkIjoiZ25vc2lzIiwicmFuZCI6IlMzS1I3WGtfUkc3R0tBYlVHQ2JiNHQ4all1UkhLVmpnc0FTeFYwME9zY1UiLCJleHAiOiIxNjUxMzY1MjUzIn0�'
+const circuitParams = {
+    blocks : Math.ceil(jwt.length / 64),// How many blocks of 512 bits are there, rounded up?
+    subStart : '"creds":"',
+    subMiddleLen : 11,
+    subEnd : '"',
+    expStart : '"exp":"',
+    expMiddleLen : 10,
+    expEnd : '"',
+    aud : '"aud":"gnosis"',
+    shiftB64 : 0 // Either 0, 1, 2, or 3 -- shifts the bits of the b64-decoded jwt by shiftB64 by adding 0, 1, 2, or 3 padding characters before decoding the jwt 
+}
+
+// A paramstring is used as a UUID for a circuit. Thus, a circuit can be generated with circuitParams and can be named ${paramString} so it is clear exactly what the circuit does
+const paramString = Buffer.from(Object.values(circuitParams).join("_")).toString("base64");
+try {
+    const provingKey = fs.readFileSync(`${paramString}.proving.key`);
+} catch(e) {
+    console.error("could not find a proving key for circuit with params", circuitParams);
+}
+
+assert([0,1,2,3].includes(circuitParams.shiftB64))
+assert(circuitParams.subStart.length + circuitParams.subMiddleLen + circuitParams.subEnd.length <= 48) // Ensure there is enough room for a 16-byte subSecret
+
+const constants = `
+const u32 SUB_START_LENGTH = ${circuitParams.subStart.length};
+const u32 SUB_END_LENGTH = ${circuitParams.subEnd.length};
+const u32 SUB_MIDDLE_LENGTH = ${circuitParams.subMiddleLen};
+const u32 SUB_LENGTH = SUB_START_LENGTH + SUB_END_LENGTH + SUB_MIDDLE_LENGTH;
+const u32 SUB_SECRET_LENGTH = 64 - SUB_MIDDLE_LENGTH; // Length of sub claim, including SUB_START and SUB_END. SHOULD NOT BE LESS THAN 16 BYTES FOR SECURE RANDOMNESS
+
+const u8[SUB_START_LENGTH] SUB_START = [ ${Buffer.from(circuitParams.subStart).join(', ')} ]; 
+const u8[SUB_END_LENGTH] SUB_END = [ ${Buffer.from(circuitParams.subEnd).join(', ')} ];
+
+const u32 EXP_START_LENGTH = ${circuitParams.expStart.length};
+const u32 EXP_END_LENGTH = ${circuitParams.expEnd.length};
+const u32 EXP_MIDDLE_LENGTH = ${circuitParams.expMiddleLen};
+const u32 EXP_LENGTH = EXP_START_LENGTH + EXP_END_LENGTH + EXP_MIDDLE_LENGTH; 
+
+const u8[EXP_START_LENGTH] EXP_START = [ ${Buffer.from(circuitParams.expStart).join(', ')} ];
+const u8[EXP_END_LENGTH] EXP_END = [ ${Buffer.from(circuitParams.expEnd).join(', ')} ];
+
+const u32 AUD_LENGTH = ${circuitParams.aud.length};
+const u8[AUD_LENGTH] AUD = [ ${Buffer.from(circuitParams.aud).join(', ')} ];
+
+const u32 JWT_BLOCKS = ${circuitParams.blocks}; // represents length of padded JWT (base64 JWT with periods, header and payload, no signature) in 512-byte multiples 
+const u32 JWT_LENGTH_PLAINTEXT = JWT_BLOCKS * 48;
+const u32 JWT_NUMBITS_PLAINTEXT = JWT_BLOCKS * 384; // how many bits a JWT is in plaintext
+
+const u32 SHIFT_BASE64 = ${circuitParams.shiftB64}; // can be either 0,1,2,3. Different versions should be compiled when it's 0,1,2, or 3
+`
 initialize().then((zokratesProvider) => {
+
 const source = `
 from "./base64" import fromBase64;
 from "./utils" import decimalStringToField, unflatten, flatten, substringAt, bool_array_to_u8_array, u8_array_to_bool_array;
@@ -23,60 +76,63 @@ import "hashes/sha256/sha256" as sha256;
 import "hashes/blake2/blake2s" as macHash;
 
 
-const u32 SUB_START_LENGTH = 9;
-// const u32 SUB_START_LENGTH = 8;
-const u32 SUB_END_LENGTH = 1;
-const u32 SUB_MIDDLE_LENGTH = 11;
-const u32 SUB_LENGTH = SUB_START_LENGTH + SUB_END_LENGTH + SUB_MIDDLE_LENGTH;
-const u32 SUB_SECRET_LENGTH = 64 - SUB_MIDDLE_LENGTH; // Length of sub claim, including SUB_START and SUB_END. SHOULD NOT BE LESS THAN 16 BYTES FOR SECURE RANDOMNESS
-// assert(SUB_SECRET_LENGTH > 15);
+${constants}
 
-const u8[SUB_START_LENGTH] SUB_START = [ 0x22, 0x63, 0x72, 0x65, 0x64, 0x73, 0x22, 0x3a, 0x22 ]; // U8 representation of "creds":" (just used for initial testing purposes, this should be changed to to the next line  later)
-
-// const u8[SUB_START_LENGTH] SUB_START = [ 0x2c, 0x22, 0x73, 0x75, 0x62, 0x22, 0x3a, 0x22 ]; // U8 representation of ,"sub":"
-const u8[SUB_END_LENGTH] SUB_END = [ 0x22 ];//, 0x2c, 0x22 ]; // U8 representation of ","
-
-const u32 EXP_START_LENGTH = 8;
-const u32 EXP_END_LENGTH = 1;
-const u32 EXP_MIDDLE_LENGTH = 10;
-const u32 EXP_LENGTH = EXP_START_LENGTH + EXP_END_LENGTH + EXP_MIDDLE_LENGTH; 
-
-const u8[EXP_START_LENGTH] EXP_START = [ 0x22, 0x2c, 0x22, 0x65, 0x78, 0x70, 0x22, 0x3a ];
-const u8[EXP_END_LENGTH] EXP_END = [ 0x22 ];//, 0x2c, 0x22 ]; // U8 representation of ","
-
-const u32 AUD_LENGTH = 14;
-const u8[AUD_LENGTH] AUD = [34,97,117,100,34,58,34,103,110,111,115,105,115,34]; // U8 representation of   "aud":"gnosis"
-
-const u32 JWT_BLOCKS = 3; // represents length of padded JWT (base64 JWT with periods, header and payload, no signature) in 512-byte multiples 
-const u32 JWT_LENGTH_PLAINTEXT = JWT_BLOCKS * 48;
-const u32 JWT_NUMBITS_PLAINTEXT = JWT_BLOCKS * 384; // how many bits a JWT is in plaintext
-
+// Formats u32[JWT_BLOCKS][16] JWT into a u8[JWT_BLOCKS*48] array of base64-decoded JWT content. This content is shifted by two bits so the payload after the . is readable
 def payloadFriendlyFormat(u32[JWT_BLOCKS][16] paddedJwt) -> u8[JWT_LENGTH_PLAINTEXT] {
-    // Flatten to bytes to search for aud, sub, and exp substring
-    u8[JWT_BLOCKS*64] flattened = flatten(paddedJwt);
-    // Convert from base64 to string search for aud, sub, and exp substring
-    u8[JWT_BLOCKS*48] plaintext = fromBase64(flattened);
-    // Shift string by two bits to account for . offset between header and payload. This will make the header undecipherable but allow the payload to be deciphered
-    bool[JWT_NUMBITS_PLAINTEXT] boolified = u8_array_to_bool_array(plaintext);
-    bool[JWT_NUMBITS_PLAINTEXT] shifted = [...boolified[2..JWT_NUMBITS_PLAINTEXT], false, false];
-    u8[JWT_BLOCKS*48] result = bool_array_to_u8_array(shifted);
-    return result;
-  }
-
-// Converts number from a 4-byte string to a u32 integer
-def main() -> u8[144] {
-  u32[3][16] paddedJwt = [[1702447730,1633112425,1332300406,1498899546,1231958062,1702447722,1668109931,1668892982,1231831673,1647530614,1496463731,1446204013,1231648617,1498961515,1231712105,1513239926],[1664248954,1231648617,1668105845,1514359094,1231834490,1395738931,1464300646,1433101107,1378907202,1500272200,1362250345,1313362228,1634495537,1433102412,1450012782,1664108116],[1699109239,1296382330,1496405353,1279478380,1699234153,1332300152,1315591544,1299863857,1298814330,1231958144,0,0,0,0,0,1336]];
-  u8[JWT_LENGTH_PLAINTEXT] formatted = payloadFriendlyFormat(paddedJwt);
-  u8[JWT_BLOCKS*64] flattened = flatten(paddedJwt);
+  // Flatten to bytes to search for aud, sub, and exp substring
+  u8[JWT_BLOCKS*64] flattened_ = flatten(paddedJwt);
+  u8[JWT_BLOCKS*64] flattened = [...[0; SHIFT_BASE64], ...flattened_[SHIFT_BASE64..JWT_BLOCKS*64]];
+  // Convert from base64 to string search for aud, sub, and exp substring
   u8[JWT_BLOCKS*48] plaintext = fromBase64(flattened);
   return plaintext;
+}
+
+// Verifies a JWT
+def main(private u32[JWT_BLOCKS][16] paddedJwt, u32[8] jwtDigest, u32[8] subCommitment, private u8[SUB_SECRET_LENGTH] subSecret, private u32 subIdx, private u32 audIdx, field expGreaterThan, private u32 expIdx) -> bool {
+  u8[JWT_LENGTH_PLAINTEXT] formatted = payloadFriendlyFormat(paddedJwt);
+  // Check that JWT contains valid sub
+  u8[SUB_START_LENGTH]  proposedSubStart   = substringAt(formatted, subIdx);
+  u8[SUB_MIDDLE_LENGTH] proposedSubMiddle  = substringAt(formatted, subIdx + SUB_START_LENGTH);
+  u8[SUB_END_LENGTH]    proposedSubEnd     = substringAt(formatted, subIdx + SUB_START_LENGTH + SUB_MIDDLE_LENGTH);
+
+  assert(proposedSubStart == SUB_START);
+  assert(proposedSubEnd == SUB_END);
+  assert(macHash(unflatten::<64,1>([...proposedSubMiddle, ...subSecret])) == subCommitment);
+
+  // Check that JWT contains valid aud
+  u8[AUD_LENGTH] proposedAud = substringAt(formatted, audIdx);
+  assert(proposedAud == AUD);
+
+  // Check that JWT contains valid exp
+  u8[EXP_START_LENGTH]  proposedExpStart  = substringAt(formatted, expIdx);
+  u8[EXP_MIDDLE_LENGTH] proposedExpMiddle = substringAt(formatted, expIdx + EXP_START_LENGTH);
+  u8[EXP_END_LENGTH]    proposedExpEnd    = substringAt(formatted, expIdx + EXP_START_LENGTH + EXP_MIDDLE_LENGTH);
+  assert(proposedExpStart == EXP_START);
+  assert(proposedExpEnd == EXP_END);
+  assert(decimalStringToField(proposedExpMiddle) > expGreaterThan);
+
+  // Check that JWT hashes to jwtDigest
+  assert(sha256(paddedJwt) == jwtDigest);
+
+  return true;
 }`
 
     // compilation
-    const artifacts = zokratesProvider.compile(source, options);
+    const artifacts = zokratesProvider.compile(source, compileOptions);
 
     // computation
-    const { witness, output } = zokratesProvider.computeWitness(artifacts, []);
+    const inputs = [
+        paddedJwt, 
+        digest, 
+        subCommitment, 
+        subSecret, 
+        subIdx, 
+        audIdx, 
+        expGreaterThan, 
+        expIdx
+    ]
+    const { witness, output } = zokratesProvider.computeWitness(artifacts, inputs);
     // const parsedOut = Buffer.concat(JSON.parse(output).map(x=>Buffer.from(x.replace("0x",""), "hex")))
     console.log(
         output,
